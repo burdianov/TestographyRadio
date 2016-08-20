3d5edfaa62b2a6b6228941bfbc57d21cbdb0a209
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-20 11:34:26 +0400

    Create boilerplate code for layouts, view holders, adapters, etc...

--- app/src/main/java/com/testography/testographyradio/fragments/StationsFragment.java
+++ app/src/main/java/com/testography/testographyradio/fragments/StationsFragment.java
@@ -3,11 +3,14 @@ package com.testography.testographyradio.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.testography.testographyradio.R;
+import com.testography.testographyradio.adapters.StationsAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,8 +62,20 @@ public class StationsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stations, container, false);
+        View v = inflater.inflate(R.layout.fragment_stations, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.recycler_stations);
+        recyclerView.setHasFixedSize(true);
+
+        StationsAdapter adapter = new StationsAdapter();
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        return v;
     }
 
 }
---
+++ app/src/main/java/com/testography/testographyradio/holders/StationViewHolder.java
@@ -0,0 +1,11 @@
+package com.testography.testographyradio.holders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class StationViewHolder extends RecyclerView.ViewHolder {
+
+    public StationViewHolder(View itemView) {
+        super(itemView);
+    }
+}
